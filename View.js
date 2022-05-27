87e3c4d75f170c66e31756c2e2d423d5510381bd
Pauolo <Pauolosavery1@gmail.com>
2022-05-27 14:02:30 +0300

    view.js добавлен метод viewResult

--- View.js
+++ View.js
@@ -4,47 +4,57 @@ const readline = require('readline').createInterface({
 })
 
 class QuizViewer {
-  viewThems(arrThems) {
+  viewThems(arrThemes) {
     return new Promise((resolve) => {
       console.log(`\n>>>>>_______Q____U______I_______Z_______<<<<<<`)
       console.log(`==============================================`)
       console.log(`>>>>>_______Let\'s get it started_______<<<<<<\n`);
-      console.log(`${arrThems.join('\n')}`);
+      console.log(`${(arrThemes.map((theme, indx)=> `${indx+1}. ${theme}`)).join('\n')}`);
       readline.question(`\nВыбери тему (введи число или напиши 'exit'):`, user_answer_theme => {
         resolve(user_answer_theme);
-        readline.close()
+        // readline.close()
       })
     })
   }
   viewQuestion(strQuestion) {
     return new Promise((resolve) => {
-      console.log(`Вы выбрали тему :`);
+      console.log(`Вы выбрали тему:`);
       console.log(`\n${strQuestion}`);
       readline.question(`\nВведи номер правильного ответа:`, user_answer_quest => {
         resolve(user_answer_quest);
-        readline.close()
+        // readline.close()
       })
     })
   }
   viewFinal(strResult) {
-    console.log(`\nПОЗДРАВЛЯЮ ВИКТОРИНА ЗАКОНЧЕНА.`);
+    console.log(`\nПОЗДРАВЛЯЮ!\n 
+    ВИКТОРИНА ЗАКОНЧЕНА.`);
     let OTBET;
     if (strResult==1){
-      OTBET = 'ответ';
+      OTBET = 'верный ответ';
     } else if (strResult>1 && strResult<5){
-      OTBET = 'ответа';
+      OTBET = 'верных ответа';
     } else {
-      OTBET = 'ответов';
+      OTBET = 'верных ответов';
     }
-    console.log(`\nТВОЙ РЕЗУЛЬТАТ: ${strResult} верных ${OTBET}!`);
+    console.log(`\nТВОЙ РЕЗУЛЬТАТ: ${strResult} ${OTBET}!`);
     readline.close()
+  }
+  viewResult(buleanResult) {
+    if (buleanResult){
+      console.log(`\nЭто правильный ответ! +1 балл`);
+    }else{
+    console.log(`\nНеправильно ¯\\_(ツ)_/¯ `);
+    }
   }
 };
 
 module.exports = QuizViewer
 
 const quiz = new QuizViewer();
-// quiz.viewThems(['1. Theme Cars', '2. Theme Champions League', '3. Theme JS']);
+// quiz.viewThems(['Theme Cars', 'Theme Champions League', 'Theme JS']);
 // quiz.viewQuestion('Question about choosing theme.');
-// quiz.viewFinal(9000);
+// quiz.viewFinal(6);
+quiz.viewResult(true);
+// quiz.viewResult(false);
 
